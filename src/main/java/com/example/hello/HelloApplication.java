4d7671269ee6855282b9f62a927bb89706b483d6
qdwuyu <2254610509@qq.com>
2019-11-26 20:40:50 +0800

    maven打包

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -3,10 +3,12 @@ package com.example.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  //标注一个主程序类，说明这是一个Spring Boot应用
 public class HelloApplication {
 
 	public static void main(String[] args) {
+		
+		//启动
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 
+//api描述
 @Api(tags = "swagger测试", value = "swagger测试接口")
+//Controller标识控制类+ResponseBody把return的结果变成JSON对象返回(免去跳转)。
 @RestController
 @RequestMapping(value = "/app")
 public class HelloController {
 
+	//地址映射
 	@RequestMapping(value = "/Hello", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	public String hello() {
 		return "hello word";
