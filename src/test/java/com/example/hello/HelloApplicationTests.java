1d221081a400647fa12d5420588ea5af3db56d72
qdwuyu <2254610509@qq.com>
2019-11-28 21:10:43 +0800

    123

--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,14 +1,13 @@
 package com.example.hello;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 //springboot单元测试
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 class HelloApplicationTests {
 
